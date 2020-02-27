583004f368969c963bf4246559312c7a31228824
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-27 14:43:00 -0600

    imported songs router

--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const songsRouter = require('../songs/songsRouter');
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
+
+server.use('/api/songs', songsRouter);
 
 server.get('/', (req, res) =>
 {
