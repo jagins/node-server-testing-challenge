62611853510ef87f97a054a42d5c601707df03ea
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-27 20:34:01 -0600

    post test passes

--- songs/songs.spec.js
+++ songs/songs.spec.js
@@ -24,3 +24,34 @@ describe('GET /api/songs', function()
             })
     })
 })
+
+describe('POST /api/songs', function()
+{
+    test('should return a 201 created', function()
+    {
+        return request(server)
+            .post('/api/songs')
+            .send({title: 'everlong', artist: 'foo fighters', album: 'the colour and the shape'})
+            .then(res =>
+            {
+                expect(res.status).toBe(201);
+            })
+    })
+
+   test('should return object with sent values', function()
+   {
+        const song = {
+            title: 'hail to the king',
+            artist: 'avenged sevenfold',
+            album: 'hail to the king'
+        }
+
+        return request(server)
+            .post('/api/songs')
+            .send(song)
+            .then(res =>
+            {
+               expect(res.body).toStrictEqual({id: res.body.id, ...song});
+            })
+   })
+})
